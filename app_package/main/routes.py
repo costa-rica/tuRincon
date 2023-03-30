2043cb2ae6092afe23533e60b664efb3d37c42d0
costa-rica <nrodrig1@gmail.com>
2023-03-30 17:19:11 +0200

    frame work with feed; still needs work

--- app_package/main/routes.py
+++ app_package/main/routes.py
@@ -7,6 +7,8 @@ import os
 import logging
 from logging.handlers import RotatingFileHandler
 from tr01_models import sess, Users, Rincons, RinconsPosts, UsersToRincons
+import shutil
+from werkzeug.utils import secure_filename
 
 
 main = Blueprint('main', __name__)
@@ -30,7 +32,8 @@ logger_main.addHandler(stream_handler)
 @main.route("/", methods=["GET","POST"])
 def home():
     logger_main.info(f"-- in home page route --")
-
+    if current_user.is_authenticated:
+        return redirect(url_for('main.rincons'))
     if request.method == "POST":
         formDict = request.form.to_dict()
 
@@ -39,10 +42,10 @@ def home():
 
 @main.route("/rincons", methods=["GET", "POST"])
 def rincons():
-    users_rincons_list = [i.rincon.name for i in current_user.rincons]
+        
+    users_rincons_list = [(i.rincons_table_id, i.rincon.name) for i in current_user.rincons]
 
     return render_template('main/rincons.html',users_rincons_list=users_rincons_list )
-
 
 
 @main.route("/search_rincons", methods=["GET", "POST"])
@@ -131,7 +134,6 @@ def search_rincons():
 def create_rincon():
 
     print(f"current_user: {current_user.username}")
-
     if request.method == "POST":
         formDict = request.form.to_dict()
 
@@ -141,14 +143,23 @@ def create_rincon():
         if formDict.get('public_checkbox') == 'true':
             public = True
         if formDict.get('rincon_name') != "":
-            new_rincon = Rincons(name= formDict.get('rincon_name'), manager_id=current_user.id, public=public)
             
+            #create_rincon
+            new_rincon = Rincons(name= formDict.get('rincon_name'), manager_id=current_user.id, public=public)
             sess.add(new_rincon)
             sess.commit()
 
+            #add current_user as member
             new_member = UsersToRincons(users_table_id = current_user.id, rincons_table_id= new_rincon.id)
             sess.add(new_member)
             sess.commit()
+
+            #create static/rincon_files/<id_rincon_name>
+            direcotry_name = f"{new_rincon.id}_{new_rincon.name}"
+            new_dir_path = os.path.join(current_app.static_folder,"rincon_files", direcotry_name)
+            print(new_dir_path)
+            os.mkdir(new_dir_path)
+
             flash("Rincon successfully created!", "success")
             
             return redirect(url_for('main.search_rincons'))
@@ -165,11 +176,85 @@ def create_rincon():
 @main.route("/rincon/<rincon_name>", methods=["GET","POST"])
 @login_required
 def rincon(rincon_name):
-    # rincon_name = request.args.get('rincon_name')
+    rincon_id = request.args.get('rincon_id')
     print("- rincon page -")
+
+    print("rincon_id: ", rincon_id)
+    rincon = sess.get(Rincons, int(rincon_id))
+    print(f"rincon: {rincon}")
+    print(f"rincon posts: {rincon.posts}")
+    rincon_posts = []
+    for i in rincon.posts:
+        temp_dict = {}
+        temp_dict['username'] = sess.get(Users,i.user_id).username
+        temp_dict['text'] = i.text
+        temp_dict['image_name_and_path'] = f"rincon_files/{rincon_id}_{rincon.name}/{i.image}"
+        temp_dict['date'] = i.time_stamp_utc.strftime("%m/%d/%y %H:%M")
+        rincon_posts.append(temp_dict)
+    print(rincon_posts)
     if request.method == "POST":
         formDict = request.form.to_dict()
         print(f"- search_rincons POST -")
         print("formDict: ", formDict)
 
-    return render_template('main/rincon_template.html', rincon_name=rincon_name)
+        requestFiles = request.files
+
+        if formDict.get('btn_delete') and formDict.get('text_delete')==rincon.name:
+            print("- ENTERED in if for btn_delete -")
+
+            return redirect(url_for('main.delete_rincon', rincon_id=rincon_id))
+        elif formDict.get('btn_post'):
+
+            # get text save a new_post entry
+            post_text = formDict.get('post_text')
+            new_post = RinconsPosts(text=post_text,user_id=current_user.id, rincon_id=rincon.id)
+            sess.add(new_post)
+            sess.commit()
+
+            # get image
+            post_image = request.files.get('add_photo_file')
+            post_image_filename = post_image.filename
+            _, file_extension = os.path.splitext(post_image_filename)
+
+            ## rename image
+            new_image_name = f"post_image_{new_post.id}.{file_extension}"
+
+            ## save to static rincon directory
+            this_rincon_dir_name = f"{rincon_id}_{rincon.name}"
+            path_to_rincon_files = os.path.join(current_app.static_folder, "rincon_files",this_rincon_dir_name)
+            post_image.save(os.path.join(path_to_rincon_files, new_image_name))
+
+            # save new image name in post entry
+            new_post.image = new_image_name
+            sess.commit()
+
+            return redirect(request.url)
+
+
+
+
+    return render_template('main/rincon_template.html', rincon_name=rincon_name, rincon_posts=rincon_posts)
+
+
+@main.route("/delete/<rincon_id>", methods=["GET"])
+@login_required
+def delete_rincon(rincon_id):
+    print("- Entered delete/rinocon route -")
+
+    rincon = sess.get(Rincons, rincon_id)
+    # remove static rincon_dir folder
+    static_dir_name = os.path.join(current_app.static_folder, "rincon_files", f"{rincon.id}_{rincon.name}")
+
+
+    if os.path.isdir(static_dir_name):
+        # os.remove(static_dir_name)
+        shutil.rmtree(static_dir_name)
+
+    # last thing: Delete rincon from rincons table
+    sess.query(Rincons).filter_by(id=rincon_id).delete()
+                
+    # delete Association Table link
+    sess.query(UsersToRincons).filter_by(users_table_id=current_user.id ,rincons_table_id=rincon_id ).delete()
+    sess.commit()
+
+    return redirect(url_for('main.rincons'))
